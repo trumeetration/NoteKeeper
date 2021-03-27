88c848d545b9842e2d85e3c5ce4982a70f56c815
trum <bardabarda228@gmail.com>
2021-03-27 12:36:11 +0500

    Notes save and view in collection

--- NoteKeeper/ViewModel.cs
+++ NoteKeeper/ViewModel.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NoteKeeper
 {
-    class ViewModel
+    class ViewModel : BaseViewModel
     {
+
+        public ObservableCollection<TextNote> TextNoteCollection { get; set; }
+
+        public ViewModel()
+        {
+            TextNoteCollection = new ObservableCollection<TextNote>();
+        }
+
+        private string _note;
+        private string _hintPhrase;
+        public string Note
+        {
+            get => _note;
+            set
+            {
+                if (_note != value)
+                {
+                    _note = value;
+                    OnPropertyChanged(nameof(Note));
+                }
+            }
+        }
+        public string HintPhrase
+        {
+            get => _hintPhrase;
+            set
+            {
+                if (_hintPhrase != value)
+                {
+                    _hintPhrase = value;
+                    OnPropertyChanged(nameof(HintPhrase));
+                }
+            }
+        }
+
+        public ICommand AddNote
+        {
+            get => new RelayCommand(() => 
+            {
+                TextNoteCollection.Add(new TextNote() {
+                    Key = HintPhrase,
+                    Value = Note
+                });
+            }, () => true);
+        }
+    }
+
+    internal class TextNote
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
     }
 }
